42f601db2d33c41443a826677ddc6cf94386faac
joe <joe-irudayaraj@gmail.com>
2024-11-23 12:10:21 -0500

    Added service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideHttpClient, withFetch} from "@angular/common/http";
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withFetch())]
+  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withFetch()), provideStore()]
 };
